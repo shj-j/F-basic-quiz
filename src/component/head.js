1b9d34be71da91e3c83e501aee83994f2744d645
杜娟 <jdu.xian@gmail.com>
2020-09-16 21:42:12 +0800

    feedback from Du Juan

--- src/component/head.js
+++ src/component/head.js
@@ -1,4 +1,5 @@
 const URL = "http://localhost:8080/users";
+// TODO feedback: 请求和rander的逻辑耦合在一起了
 const getData = () => {
   fetch(`${URL}/1`, {
     method: "GET",
@@ -13,9 +14,10 @@ const getData = () => {
       return Promise.reject(new Error(response.statusText));
     })
     .then((data) => {
+      // TODO feedback: 不建议用标签选择器，未来新添加的标签都会被写入html
       $("h2").html(`MY NAME IS ${data.name} AND THIS IS MY RESUME/CV`);
       $(".image-size").attr("src", data.avatar);
-      $("#user-description").html(data.description)
+      $("#user-description").html(data.description);
     });
 };
 
